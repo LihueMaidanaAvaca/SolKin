b87dc082b6d755282bb765f7f0ee66b5e4b13d08
lmaidana <lmaidana@bienesdelestado.gob.ar>
2023-12-18 15:24:34 -0300

    change on spellwave size 1

--- src/components/SpellWave.js
+++ src/components/SpellWave.js
@@ -9,33 +9,33 @@ const SpellWave = ({ kinMaya }) => {
             style={{ maxWidth: '500px', margin: 'auto' }} // Ajusta el maxWidth según tus necesidades
         >
             <div>
-                <img src={kinMaya.imgWave} alt="Hun" style={{ width: '125px', height: 'auto' }} />
+                <img src={kinMaya.imgWave} alt="Hun" style={{ width: '100px', height: 'auto' }} />
             </div>
             <div style={{ display: "flex", justifyContent: "space-between" }} // Distribuye el espacio entre los elementos
             >
-                <img src={imgSeals(magnetNum + 1)} alt="Ka" style={{ width: '100px', height: 'auto' }} />
+                <img src={imgSeals(magnetNum + 1)} alt="Ka" style={{ width: '75px', height: 'auto' }} />
                 <div >
-                    <img src={imgSeals(magnetNum + 12)} alt="Oxlahun" style={{ width: '100px', height: 'auto' }} />
-                    <img src={imgSeals(magnetNum + 11)} alt="Lahat" style={{ width: '100px', height: 'auto' }} />
+                    <img src={imgSeals(magnetNum + 12)} alt="Oxlahun" style={{ width: '75px', height: 'auto' }} />
+                    <img src={imgSeals(magnetNum + 11)} alt="Lahat" style={{ width: '75px', height: 'auto' }} />
                 </div>
             </div>
             <div style={{ display: "flex", justifyContent: "space-between" }} // Distribuye el espacio entre los elementos
             >
-                <img src={imgSeals(magnetNum + 2)} alt="Ox" style={{ width: '100px', height: 'auto' }} />
-                <img src={imgSeals(magnetNum + 10)} alt="Onda Maya" style={{ width: '100px', height: 'auto' }} />
+                <img src={imgSeals(magnetNum + 2)} alt="Ox" style={{ width: '75px', height: 'auto' }} />
+                <img src={imgSeals(magnetNum + 10)} alt="Buluk" style={{ width: '75px', height: 'auto' }} />
             </div>
             <div style={{ display: "flex", justifyContent: "space-between" }} // Distribuye el espacio entre los elementos
             >
-                <img src={imgSeals(magnetNum + 3)} alt="Kan" style={{ width: '100px', height: 'auto' }} />
-                <img src={imgSeals(magnetNum + 9)} alt="Onda Maya" style={{ width: '100px', height: 'auto' }} />
+                <img src={imgSeals(magnetNum + 3)} alt="Kan" style={{ width: '75px', height: 'auto' }} />
+                <img src={imgSeals(magnetNum + 9)} alt="Lahun" style={{ width: '75px', height: 'auto' }} />
             </div>
             <div style={{ display: "flex", justifyContent: "space-between" }} // Distribuye el espacio entre los elementos
             >
-                <img src={imgSeals(magnetNum + 4)} alt="Ho" style={{ width: '100px', height: 'auto' }} />
-                <img src={imgSeals(magnetNum + 5)} alt="Onda Maya" style={{ width: '100px', height: 'auto' }} />
-                <img src={imgSeals(magnetNum + 6)} alt="Onda Maya" style={{ width: '100px', height: 'auto' }} />
-                <img src={imgSeals(magnetNum + 7)} alt="Onda Maya" style={{ width: '100px', height: 'auto' }} />
-                <img src={imgSeals(magnetNum + 8)} alt="Onda Maya" style={{ width: '100px', height: 'auto' }} />
+                <img src={imgSeals(magnetNum + 4)} alt="Ho" style={{ width: '75px', height: 'auto' }} />
+                <img src={imgSeals(magnetNum + 5)} alt="Uak" style={{ width: '75px', height: 'auto' }} />
+                <img src={imgSeals(magnetNum + 6)} alt="Uuk" style={{ width: '75px', height: 'auto' }} />
+                <img src={imgSeals(magnetNum + 7)} alt="Uahak" style={{ width: '75px', height: 'auto' }} />
+                <img src={imgSeals(magnetNum + 8)} alt="Bolon" style={{ width: '75px', height: 'auto' }} />
             </div>
             {/* ...resto de tus imágenes */}
         </div>
